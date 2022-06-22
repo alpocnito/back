6605d16c9263fdea7336adfcc9c94a8df8f83dbb
r.zaripov <r.zaripov@atb-e.ru>
2022-06-22 12:22:22 +0300

    stable

---
+++ controllers/variables.js
@@ -0,0 +1,5 @@
+const events = require("events");
+module.exports.CHIRP_API_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5X2lkIjoiMzFlNjA4N2EtMjY1MC00ZjEwLTk4MGQtYjc1YzkxMTZjNTBhIiwiYXVkIjoiYXMiLCJpc3MiOiJhcyIsIm5iZiI6MTY1NDU4NDg3MSwic3ViIjoiYXBpX2tleSJ9.Sn5FBFnyjtPRJ0JBY_eHI_VVk25wdlTkb4E7UeaDx30";
+module.exports.emitter = new events.EventEmitter();
+module.exports.CHIRP_URL = "10.15.30.46";
